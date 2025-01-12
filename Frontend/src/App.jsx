ac9e4e021bf1cdf97c12acff92a9176e29f8f091
Kundan Kumar <rajkundan9535@gmail.com>
2025-01-13 01:21:10 +0530

    navbar-updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,8 @@ import Rating from "./components/Rating";
 import FAQPage from "./components/FAQPage";
 import Footer from "./components/Footer";
 import FeaturedCompanies from "./components/FeaturedCompanies";
+import Navbar from "./components/Navbar";
+
 // import QualificationPopup from "./components/QualificationPopup";
 
 // >>>>>>> 5202aa8e92e7789bc0d53c732e5f2fa68c77ddde
@@ -16,9 +18,9 @@ import FeaturedCompanies from "./components/FeaturedCompanies";
 function App() {
   return (
     <div className={styles.jobportalContainer}>
-      {/* <div className={styles.navbar}>
+      <div className="container">
         <Navbar />
-      </div> */}
+      </div>
       <div className={styles.heroSection}>
         <HeroSection />
       </div>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,26 +1,76 @@
-import React from "react";
-import styles from "../styles/Navbar.module.css";
+import { MenuIcon, Search, X } from "lucide-react";
+import React, { useState } from "react";
+
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className={styles.navBar}>
-      <h1 className={styles.logo}>Quiety</h1>
-      <ul className={styles.navLinks}>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Find Jobs</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-      <a href="#">
-        <button className={styles.getStarted}>Get Started</button>
-      </a>
+    <nav className="sticky top-0 z-50 max-w-6xl mx-auto">
+      <div className=" px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          {/* Logo Section */}
+          <div className="mb-1">
+            <img
+            className="h-8" 
+            src="./logo.png" alt="" />
+          </div>
+
+          {/* Hamburger Icon for Mobile View */}
+          <div className="lg:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className=" hover:text-blue-300 focus:outline-none"
+            >
+              {isOpen ? <X size={24} /> : <MenuIcon size={24} />}
+            </button>
+          </div>
+
+          {/* Links Section */}
+          <div
+            className={`lg:flex items-center space-x-6 ${
+              isOpen ? "block" : "hidden"
+            } lg:block`}
+          >
+            <div className="flex flex-col lg:flex-row lg:space-x-9 space-y-2 lg:space-y-0 p-2 m-4">
+              <a href="#" className="hover:text-[#FF6D34]">
+                Courses
+              </a>
+              <a href="#" className="hover:text-[#FF6D34]">
+                Jobs
+              </a>
+              <a href="#" className="hover:text-[#FF6D34]">
+                Shops
+              </a>
+              <a href="#" className="hover:text-[#FF6D34]">
+                Discover
+              </a>
+              <a href="#" className="hover:text-[#FF6D34]">
+                Degree Program
+              </a>
+            </div>
+            <div className="flex flex-col lg:flex-row lg:space-x-4 space-y-2 lg:space-y-0">
+              <button className="px-4 py-3 rounded-full bg-[#FF6D34]">
+                For Colleges
+              </button>
+              <button className="px-4 py-3 border border-[#29CC7A] rounded-full hover:bg-[#FF6D34]">
+                Need Help?
+              </button>
+              <div className="items-center flex">
+              <Search size={30}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
 
 export default Navbar;
+
+
+
+
+
+
